321769aa2953397a6001d8e5c4a580d52ea9c328
Saumya Singh <singhsaumya201@gmail.com>
2025-01-22 00:47:00 +0530

    env done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Header from './components/Header/Header'
+import Header from './components/Headers/Header'
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import Home from './Home';
